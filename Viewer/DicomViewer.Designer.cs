7eaf44904a9446055c06f795c91c338cc900cc5e
Sharmin Jose <Sharmin.Jose@vinsolutions.com>
2019-01-03 17:18:13 -0600

    Added a Viewer Project for viewing Dicom File

---
+++ Viewer/DicomViewer.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace DicomFileViewer
+{
+    partial class DicomViewer
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtFilePath = new System.Windows.Forms.TextBox();
+            this.btnBrowse = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtFilePath
+            // 
+            this.txtFilePath.Location = new System.Drawing.Point(37, 45);
+            this.txtFilePath.Name = "txtFilePath";
+            this.txtFilePath.ReadOnly = true;
+            this.txtFilePath.Size = new System.Drawing.Size(694, 26);
+            this.txtFilePath.TabIndex = 0;
+            // 
+            // btnBrowse
+            // 
+            this.btnBrowse.Location = new System.Drawing.Point(737, 41);
+            this.btnBrowse.Name = "btnBrowse";
+            this.btnBrowse.Size = new System.Drawing.Size(81, 35);
+            this.btnBrowse.TabIndex = 1;
+            this.btnBrowse.Text = "&Browse";
+            this.btnBrowse.UseVisualStyleBackColor = true;
+            this.btnBrowse.Click += new System.EventHandler(this.btnBrowse_Click);
+            // 
+            // Viewer
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1532, 1098);
+            this.Controls.Add(this.btnBrowse);
+            this.Controls.Add(this.txtFilePath);
+            this.Name = "Viewer";
+            this.Text = "Dicom File Viewer";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtFilePath;
+        private System.Windows.Forms.Button btnBrowse;
+    }
+}
+
---
+++ Viewer/DicomViewer.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+using Dicom;
+
+namespace DicomFileViewer
+{
+    public partial class DicomViewer : Form
+    {
+        public DicomViewer()
+        {
+            InitializeComponent();
+        }
+
+        private void btnBrowse_Click(object sender, EventArgs e)
+        {
+            txtFilePath.Text = string.Empty;
+            using (var ofd = new OpenFileDialog())
+            {
+                ofd.InitialDirectory = "D:\\";
+                ofd.Filter = @"dicom files (*.dcm)|*.dcm|All files(*.*)|*.*";
+                ofd.FilterIndex = 2;
+                ofd.RestoreDirectory = true;
+
+                if (ofd.ShowDialog() == DialogResult.OK)
+                {
+                    var filePath = ofd.FileName;
+                    txtFilePath.Text = ofd.FileName;
+                    var dicomFileInfo = GetDicomFileInfo(filePath);
+                }
+            }
+        }
+
+        private IEnumerable<string> GetDicomFileInfo(string path)
+        {
+            var file = DicomFile.Open(path);
+            var patientIds = file.Dataset.GetValues<string>(DicomTag.PatientID);
+            return patientIds;
+        }
+
+        /*
+        /// <summary>
+        /// Gets and prints information about DICOM file.
+        /// </summary>
+        /// <param name="filePath">Path to a DICOm file.</param>
+        private List<DicomDataElementMetadata> GetDicomFileInfo(string filePath)
+        {
+            var dicomFileInfo = new List<DicomDataElementMetadata>();
+
+            // open DICOM file
+            using (var file = new DicomFile(filePath, true))
+            {
+                // show file name and page count
+                Console.WriteLine($@"File: {Path.GetFileName(filePath)} Page count: {file.Pages.Count}");
+                
+                // get DICOM file metadata
+                var fileMetadata = new DicomFrameMetadata(file);
+
+                // for each metadata node
+                foreach (var children in fileMetadata)
+                {
+                    dicomFileInfo.Add(GetMetadataNodeInfo(children));
+                }
+            }
+
+            return dicomFileInfo;
+        }
+
+        /// <summary>
+        /// Return metadata node if it is only a Metadata node. Otherwise recursively get the information
+        /// </summary>
+        /// <param name="node">Metadata node.</param>
+        private DicomDataElementMetadata GetMetadataNodeInfo(MetadataNode node)
+        {
+            // if node is DicomDataElementMetadata
+            if (node is DicomDataElementMetadata dataElementMetadata)
+            {
+                return dataElementMetadata;
+            }
+
+            // show children info
+            foreach (var children in node)
+                GetMetadataNodeInfo(children);
+
+            throw new Exception("Node is not DicomDataElementMetadata");
+        }
+        */
+    }
+}
